084999ab01caa0be9a251fd3224050feeb098e07
Timm Bremus <tbremus@bremus-solutions.de>
2018-08-31 17:29:31 +0200

    New exception for mandatory attributes

---
+++ BSolutions.Brecons/BSolutions.Brecons.Core/Exceptions/MandatoryAttributeException.cs
@@ -0,0 +1,24 @@
+﻿namespace BSolutions.Brecons.Core.Exceptions
+{
+    using System;
+
+    public class MandatoryAttributeException : Exception
+    {
+        public string Attribute { get; set; }
+
+        public Type TagHelper { get; set; }
+
+        public MandatoryAttributeException(string attribute)
+            : base($"The '{attribute}' attribute is mandatory and must be set.")
+        {
+            this.Attribute = attribute;
+        }
+
+        public MandatoryAttributeException(string attribute, Type tagHelper)
+            : base($"The '{attribute}' attribute of the '{tagHelper.Name}' is mandatory and must be set.")
+        {
+            this.Attribute = attribute;
+            this.TagHelper = tagHelper;
+        }
+    }
+}
